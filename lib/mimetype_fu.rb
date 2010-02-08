c47b701770e3e09273d97d632c9f0c1f23d9cc19
J.D. Hollis <jd@densityofspace.com>
2010-02-08 14:12:59 -0500

    Use file --mime-type instead of file --mime.

--- lib/mimetype_fu.rb
+++ lib/mimetype_fu.rb
@@ -7,17 +7,17 @@ class File
     case file
     when File, Tempfile
       unless RUBY_PLATFORM.include? 'mswin32'
-        mime = `file --mime -br "#{file.path}"`.strip
+        mime = `file --mime-type -br "#{file.path}"`.strip
       else
         mime = EXTENSIONS[File.extname(file.path).gsub('.','').downcase.to_sym]
       end
     when String
       mime = EXTENSIONS[(file[file.rindex('.')+1, file.size]).downcase.to_sym] unless file.rindex('.').nil?
     when StringIO
-      temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, "wb")
+      temp = File.open(Dir.tmpdir + '/upload_file.' + Process.pid.to_s, 'wb')
       temp << file.string
       temp.close
-      mime = `file --mime -br "#{temp.path}"`
+      mime = `file --mime-type -br "#{temp.path}"`
       mime = mime.gsub(/^.*: */,"")
       mime = mime.gsub(/;.*$/,"")
       mime = mime.gsub(/,.*$/,"")
